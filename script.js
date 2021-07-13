b6732a1234606f5ddee40fc369fc56d4df3896a8
Malik Washington <kingmalikwashington@gmail.com>
2021-07-13 15:14:06 -0500

    added variables and if statments to javascript

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("Welcome Tigers Fans")
+var name=prompt('What is the number one Tigers fan name');
+alert('Hello,' + name);
+alert("Welcome Tigers Fans");
+var num1=prompt('How much you like the Memphis Tigers Football Team from 1-10');
+if (userAnswer <10){
+alert('Bad answer');
+}
+var user_answer =10
