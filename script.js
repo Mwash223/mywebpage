cf34fcebabec38e37865b87622a79a1950ab0dc5
Malik Washington <kingmalikwashington@gmail.com>
2021-07-13 18:43:22 -0500

    added an alert

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var name=prompt('What is the number one Tigers fan name');
-alert('Hello,' + name);
+alert('Hello, ' + name);
 alert("Welcome Tigers Fans");
 var num1=prompt('How much you like the Memphis Tigers Football Team from 1-10');
-if (userAnswer <10){
-alert('Bad answer');
+if (userAnswer =10){
+alert('Good Answer');
 }
-var user_answer =10
+
