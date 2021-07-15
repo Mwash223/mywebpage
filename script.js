18cf9ab18079b1a3119ba8c5a752f2507d19ba50
Malik Washington <kingmalikwashington@gmail.com>
2021-07-14 21:37:31 -0500

    added while loops and if statements

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 var name=prompt('What is the number one Tigers fan name');
 alert('Hello, ' + name);
 alert("Welcome Tigers Fans");
-var num1=prompt('How much you like the Memphis Tigers Football Team from 1-10');
-if (userAnswer =10){
-alert('Good Answer');
+function tigerRating(){
+    var num1=prompt('How much you like the Memphis Tigers Football Team from 1-10')
+while (num1 <5){
+ 
+
+ if(num1  < 5){
+    alert('Bye Tigers do not like liars, ' + name);
+    var num1=prompt('TRY AGAIN!!How much you like the Memphis Tigers Football Team from 1-10');
 }
+}
+if (num1 ==10){
+    alert('Good Answer, ' + name);
+    }
+}
+tigerRating();
+
+ 
 
